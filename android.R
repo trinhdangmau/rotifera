cc85d4bc051713bc3f5611ebdddb9a8061d568d0
trinhdangmau <trinhdangmau@users.noreply.github.com>
2014-11-25 02:41:49 +0700

    Update android.R

--- android.R
+++ android.R
@@ -5,13 +5,13 @@ biolabs<-function(){
 }
 #### Download data set ###############
 download.data<-function(){
-  source.dir<-paste(getwd(), "R-project", sep="")
-  data.dir<-paste(getwd(), "Image", sep="")
+  source.dir<-paste(getwd(), "/R-project", sep="")
+  data.dir<-paste(getwd(), "/Image", sep="")
   dir.create(data.dir)
   dir.create(source.dir)
   
   datafile<-paste(data.dir, "/VnRotifera.db.sqlite", sep="")
-  sourcefile<-paste(source.dir, "biolab.r", sep="")
+  sourcefile<-paste(source.dir, "/biolab.r", sep="")
   
   download.file("http://anthaiphusteel.com/dmautrinh/VnRotifera.db.sqlite", destfile=datafile, method = "curl")
   download.file("https://raw.githubusercontent.com/trinhdangmau/rotifera/master/biolabs.r", destfile=sourcefile, method="curl")
