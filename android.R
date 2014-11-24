153a7049ad37a02996d200acdc61f9ddb1f2584c
trinhdangmau <trinhdangmau@users.noreply.github.com>
2014-11-25 02:58:31 +0700

    Update android.R

--- android.R
+++ android.R
@@ -16,5 +16,6 @@ download.data<-function(){
   download.file("http://anthaiphusteel.com/dmautrinh/VnRotifera.db.sqlite", destfile=datafile, method = "curl")
   download.file("https://raw.githubusercontent.com/trinhdangmau/rotifera/master/biolabs.r", destfile=sourcefile, method="curl")
   
+  setwd(source.dir)
   source(sourcefile)
 }
